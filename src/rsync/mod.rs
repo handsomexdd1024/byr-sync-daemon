c4563ff72f791305b021d66162a467d176fe24ed
Billy Yang <billy@byr.ink>
2024-01-27 11:35:17 +0800

    enhance rsync module

--- src/rsync/mod.rs
+++ src/rsync/mod.rs
@@ -1,12 +1,42 @@
 use std::path::{Path, PathBuf};
 use tokio::process::*;
+use std::error::Error;
+use std::fmt::Display;
+use tokio::io::AsyncBufReadExt;
 
-pub async fn rsync<P: AsRef<Path>>(src: P, dst: P) -> Result<(), Box<dyn std::error::Error>> {
-    let src = src.as_ref();
-    let dst = dst.as_ref();
+#[derive(Debug)]
+pub struct RsyncError {
+    pub msg: String,
+    pub status: Option<i32>,
+}
+
+impl Display for RsyncError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "RsyncError with code {}\nMessage: {}", self.status.unwrap_or(-1), self.msg)
+    }
+}
+
+impl Error for RsyncError {}
+
+pub async fn rsync<P: AsRef<Path>>(
+    src: P,
+    host: &String,
+    dst_path: P
+) -> Result<(), Box<dyn Error>> {
     let mut cmd = Command::new("rsync");
-    cmd.arg("-arvh").arg(src).arg(dst);
-    let mut rclone_ps = cmd.spawn()?;
-    rclone_ps.wait().await.unwrap();
-    Ok(())
+    cmd.arg("-arvh");
+    cmd.arg(src.as_ref());
+    cmd.arg(format!("{}:{}", host, dst_path.as_ref().display()));
+    let mut child = cmd.spawn()?;
+    let status = child.wait().await?;
+    match status.success() {
+        true => Ok(()),
+        false => {
+            let child_stderr = child.stderr.take().unwrap();
+            let mut reader = tokio::io::BufReader::new(child_stderr);
+            let mut line = String::new();
+            reader.read_line(&mut line).await?;
+            Err(Box::new(RsyncError { msg: line , status: status.code()}))
+        }
+    }
 }
